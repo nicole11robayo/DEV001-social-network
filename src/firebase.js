6d5f0423abab08055acc1512309c4aae3b7a2114
Keixys <reyeskeixysmar@gmail.com>
2022-11-11 14:22:31 -0500

    sube la tarjeta de registro, instalacion del firebase y de  node.js

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyA3maUa17yJd5U-6oLPwJezQ7NfOP1Ycnw',
+  authDomain: 'laboratorians-music-2.firebaseapp.com',
+  projectId: 'laboratorians-music-2',
+  storageBucket: 'laboratorians-music-2.appspot.com',
+  messagingSenderId: '603110199557',
+  appId: '1:603110199557:web:1a7d20d7f0905017db276c',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
